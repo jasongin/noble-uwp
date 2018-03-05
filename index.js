39f9b119623dab55915bdb239434acc2c69a1fec
Semih Energin <semihenergin@gmail.com>
2018-03-05 12:52:43 -0800

    Extending UWP specific methods. Format unification (#51)

--- index.js
+++ index.js
@@ -2,17 +2,25 @@
 
 const os = require('os');
 
+// noble-uwp acts as a shim to noble. If Windows >= 10.0.15014, it will bind UWP to noble interface
+// Otherwise, it will either fail (on Windows), or revert back to noble (noble does not have default-Windows support)
 if (os.platform() === 'win32') {
 	const ver = os.release().split('.').map(Number);
 	if (!(ver[0] > 10 ||
-			(ver[0] === 10 && ver[1] > 0) ||
-			(ver[0] === 10 && ver[1] === 0 && ver[2] >= 15014))) {
+		(ver[0] === 10 && ver[1] > 0) ||
+		(ver[0] === 10 && ver[1] === 0 && ver[2] >= 15014))) {
 		throw new Error("Noble UWP bindings require Windows >= 10.0.15014.");
 	}
 
 	const Noble = require('noble/lib/noble');
 	const uwpBindings = require('./lib/bindings.js');
-	module.exports = new Noble(uwpBindings);
+	const uwpAddition = require('./lib/uwpAddition.js');
+
+	var nobleInstance = new Noble(uwpBindings);
+	uwpAddition.init(nobleInstance._bindings);
+	nobleInstance.uwp = uwpAddition;
+
+	module.exports = nobleInstance;
 } else {
-	module.exports = require('noble');;
+	module.exports = require('noble');
 }
